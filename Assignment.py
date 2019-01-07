9049c98530e38de2ea892566e578e4d3998b6022
santhosh kumar <sandysanthosh101@gmail.com>
2019-01-07 22:18:57 +0530

    Task for Assignment

---
+++ Assignment.py
@@ -0,0 +1,17 @@
+""" This
+ is
+ a casting"""
+int(5.6)
+print('ranjith')  #using single quotation
+b = "     Hello World!       "
+print(b[2:6]) #print the character position of 2 to 6
+print(b.strip()) #rremoves whitespace front or end
+
+print(len(b)) #print length of b
+print(b.lower()) #print lower similarly upper
+print(b.replace("Hello","ranjith")) #replace a word using variable and replace keyword
+print(b.split())
+
+print("Enter your name:")
+x = input()
+print("Hello, " + x) #user to enter name  from command
